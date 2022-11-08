362cbd61f710ddda57537d3b5cbca23626b4c16f
ValerkaIVIX <valerka_ivix@mail.ru>
2022-11-08 18:20:46 +0300

    Добавлена возможность чтения всех файлов из папки

--- main.py
+++ main.py
@@ -1,26 +1,63 @@
 import os
 import re
-
+from tqdm import tqdm
+from time import sleep
 
 def ReadFile(file):
     data = []
+    data1 = []
     with open(file, 'r') as f:
         for line in f:
-            data.append(line)
+            data1.append(line)
+    data.append(data1)
+    return data
+
+
+def ReadFiles(filepath):
+    data = []
+    for filename in os.listdir(filepath):
+        with open(filepath + "\\" + filename, 'r') as f:
+            file = []
+            for line in f:
+                file.append(line)
+            data.append(file)
+    return data
+
+
+def TotalRead(filepath):
+    data = []
+    if os.path.isfile(filepath):
+        data = ReadFile(filepath)
+    else:
+        data = ReadFiles(filepath)
     return data
 
 
 def SearchError(log, word):
-    filteredLog = [x for x in log if re.search(word, x)]
+    filteredLog = []
+    for l in log:
+        filtered = [x for x in l if re.search(word, x)]
+        filteredLog.append(filtered)
     return filteredLog
 
 
 def WriteFile(newlog):
-    with open('parced.txt', 'w') as f:
-        for line in newlog:
-            f.write(line + '\n')
+    n = 0
+    for l in newlog:
+        n += 1
+        with open('log' + str(n) + '.txt', 'w') as f:
+            for line in l:
+                f.write(line + '\n')
 
-log = ReadFile(input('Введите путь до файла:'))
-errorLog = SearchError(log, input('Введите слово для поиска:'))
+
+log = TotalRead(input('Введите путь до файла или папки:'))
+errorLog = SearchError(log, input('Введите слово для парсинга:'))
+
+for i in tqdm(range(0, 100), ncols = 100,
+               desc ="Обработка файла"):
+    sleep(.01)
+
 WriteFile(errorLog)
-print(*errorLog, sep='\n')
+isEnd = input("Успешно! \nЧтобы закрыть программу нажмите Enter")
+if isEnd:
+    quit()
